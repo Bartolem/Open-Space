a1a8df8ad15450f75929f14e4b7fc5a123fa5e40
Bartolem <97914472+Bartolem@users.noreply.github.com>
2022-05-20 10:44:55 +0200

    JavaScript additions

--- scripts.js
+++ scripts.js
@@ -1,14 +1,10 @@
-// function disableInput(){
-//     password.disabled = true;
-//     submit.disabled = true;
-// }
 let password = document.getElementById("password").value;
 let submit = document.querySelector(".submitPassword");
 let checkButton = document.querySelectorAll(".check-button");
 let lever = document.querySelectorAll(".lever");
 let launch = document.getElementById("launch");
 
-function disable(){
+function disable() {
     checkButton.forEach(button => {
         button.setAttribute("disabled", "true");
     });
@@ -17,34 +13,72 @@ function disable(){
     });
     launch.setAttribute("disabled", "true");
 }
-function enable(){
+
+function enable() {
     checkButton.forEach(button => {
         button.removeAttribute("disabled", "false");
     });
     lever.forEach(button => {
         button.removeAttribute("disabled", "false");
     });
-    launch.removeAttribute("disabled", "false");
+
+    checkButton.forEach( button => {
+        button.onchange = unlockLaunch;
+    });
+    lever.forEach( button => {
+        button.onchange = unlockLaunch;
+    });
 }
-function disablePassword(){
+
+function disablePassword() {
     submit.setAttribute("disabled", "true");
     document.getElementById("password").disabled = true;
 }
-function checkPassword(){
+
+function unlockLaunch() {
+    let checkedCheckButton;
+    let checkedLever;
+
+    for (let i = 0; i < checkButton.length; i++) {
+        let element = checkButton[i];
+
+        if (!element.checked) {
+            checkedCheckButton = false;
+        }
+    }
+
+    for (let i = 0; i < lever.length; i++) {
+        let element = lever[i];
+
+        if (!element.value < 10) {
+            checkedLever = false;
+        }
+    }
+
+    if (checkedCheckButton && checkedLever) {
+        launch.removeAttribute("disable", "false");
+    }
+    else {
+        launch.setAttribute("disable", "true");
+    }
+}
+
+function checkPassword() {
     let password = document.getElementById("password").value;
 
-    if (password === "TrustNo1"){
+    if (password === "TrustNo1") {
         enable();
         disablePassword();
     }
-    else if (password === ""){
+    else if (password === "") {
         console.log("Enter the password");
     }
     else {
         console.log("Wrong password!");
     }
 }
-function start(){
+
+function start() {
     disable();
     submit.addEventListener("click",() => checkPassword());
 }
